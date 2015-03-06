f548e4d46a509d11965a97e6f631cabfe37cd579
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2015-03-05 20:09:39 -0400

    Improving register file, still broken

--- Register-File/registerFile.v
+++ Register-File/registerFile.v
@@ -1,10 +1,11 @@
 module register_file (
 	output reg [31:0] dataRS,
 	output reg [31:0] dataRT,
-	input reg [4:0] RD,
-	input reg [4:0] RS,
-	input reg [4:0] RT,
-	input reg [31:0] dataRD,
+	input [4:0] RD,
+	input [4:0] RS,
+	input [4:0] RT,
+	input [31:0] dataRD,
+	input RW,
 	input Clk);
 
 	reg [31:0] LE_RD;
@@ -44,38 +45,38 @@ module register_file (
 
 	decoder_5_32 decoderRD (LE_RD,RD);
 
-	Register32 register0 (Q0,dataRD,LE_RD[0],0,Clk);
-	Register32 register1 (Q1,dataRD,LE_RD[1],0,Clk);
-	Register32 register2 (Q2,dataRD,LE_RD[2],0,Clk);
-	Register32 register3 (Q3,dataRD,LE_RD[3],0,Clk);
-	Register32 register4 (Q4,dataRD,LE_RD[4],0,Clk);
-	Register32 register5 (Q5,dataRD,LE_RD[5],0,Clk);
-	Register32 register6 (Q6,dataRD,LE_RD[6],0,Clk);
-	Register32 register7 (Q7,dataRD,LE_RD[7],0,Clk);
-	Register32 register8 (Q8,dataRD,LE_RD[8],0,Clk);
-	Register32 register9 (Q9,dataRD,LE_RD[9],0,Clk);
-	Register32 register10 (Q10,dataRD,LE_RD[10],0,Clk);
-	Register32 register11 (Q11,dataRD,LE_RD[11],0,Clk);
-	Register32 register12 (Q12,dataRD,LE_RD[12],0,Clk);
-	Register32 register13 (Q13,dataRD,LE_RD[13],0,Clk);
-	Register32 register14 (Q14,dataRD,LE_RD[14],0,Clk);
-	Register32 register15 (Q15,dataRD,LE_RD[15],0,Clk);
-	Register32 register16 (Q16,dataRD,LE_RD[16],0,Clk);
-	Register32 register17 (Q17,dataRD,LE_RD[17],0,Clk);
-	Register32 register18 (Q18,dataRD,LE_RD[18],0,Clk);
-	Register32 register19 (Q19,dataRD,LE_RD[19],0,Clk);
-	Register32 register20 (Q20,dataRD,LE_RD[20],0,Clk);
-	Register32 register21 (Q21,dataRD,LE_RD[21],0,Clk);
-	Register32 register22 (Q22,dataRD,LE_RD[22],0,Clk);
-	Register32 register23 (Q23,dataRD,LE_RD[23],0,Clk);
-	Register32 register24 (Q24,dataRD,LE_RD[24],0,Clk);
-	Register32 register25 (Q25,dataRD,LE_RD[25],0,Clk);
-	Register32 register26 (Q26,dataRD,LE_RD[26],0,Clk);
-	Register32 register27 (Q27,dataRD,LE_RD[27],0,Clk);
-	Register32 register28 (Q28,dataRD,LE_RD[28],0,Clk);
-	Register32 register29 (Q29,dataRD,LE_RD[29],0,Clk);
-	Register32 register30 (Q30,dataRD,LE_RD[30],0,Clk);
-	Register32 register31 (Q31,dataRD,LE_RD[31],0,Clk);
+	Register32 register0 (Q0,dataRD,LE_RD[0] && RW,1,Clk);
+	Register32 register1 (Q1,dataRD,LE_RD[1] && RW,0,Clk);
+	Register32 register2 (Q2,dataRD,LE_RD[2] && RW,0,Clk);
+	Register32 register3 (Q3,dataRD,LE_RD[3] && RW,0,Clk);
+	Register32 register4 (Q4,dataRD,LE_RD[4] && RW,0,Clk);
+	Register32 register5 (Q5,dataRD,LE_RD[5] && RW,0,Clk);
+	Register32 register6 (Q6,dataRD,LE_RD[6] && RW,0,Clk);
+	Register32 register7 (Q7,dataRD,LE_RD[7] && RW,0,Clk);
+	Register32 register8 (Q8,dataRD,LE_RD[8] && RW,0,Clk);
+	Register32 register9 (Q9,dataRD,LE_RD[9] && RW,0,Clk);
+	Register32 register10 (Q10,dataRD,LE_RD[10] && RW,0,Clk);
+	Register32 register11 (Q11,dataRD,LE_RD[11] && RW,0,Clk);
+	Register32 register12 (Q12,dataRD,LE_RD[12] && RW,0,Clk);
+	Register32 register13 (Q13,dataRD,LE_RD[13] && RW,0,Clk);
+	Register32 register14 (Q14,dataRD,LE_RD[14] && RW,0,Clk);
+	Register32 register15 (Q15,dataRD,LE_RD[15] && RW,0,Clk);
+	Register32 register16 (Q16,dataRD,LE_RD[16] && RW,0,Clk);
+	Register32 register17 (Q17,dataRD,LE_RD[17] && RW,0,Clk);
+	Register32 register18 (Q18,dataRD,LE_RD[18] && RW,0,Clk);
+	Register32 register19 (Q19,dataRD,LE_RD[19] && RW,0,Clk);
+	Register32 register20 (Q20,dataRD,LE_RD[20] && RW,0,Clk);
+	Register32 register21 (Q21,dataRD,LE_RD[21] && RW,0,Clk);
+	Register32 register22 (Q22,dataRD,LE_RD[22] && RW,0,Clk);
+	Register32 register23 (Q23,dataRD,LE_RD[23] && RW,0,Clk);
+	Register32 register24 (Q24,dataRD,LE_RD[24] && RW,0,Clk);
+	Register32 register25 (Q25,dataRD,LE_RD[25] && RW,0,Clk);
+	Register32 register26 (Q26,dataRD,LE_RD[26] && RW,0,Clk);
+	Register32 register27 (Q27,dataRD,LE_RD[27] && RW,0,Clk);
+	Register32 register28 (Q28,dataRD,LE_RD[28] && RW,0,Clk);
+	Register32 register29 (Q29,dataRD,LE_RD[29] && RW,0,Clk);
+	Register32 register30 (Q30,dataRD,LE_RD[30] && RW,0,Clk);
+	Register32 register31 (Q31,dataRD,LE_RD[31] && RW,0,Clk);
 
 	mux_32x1 muxRS (dataRS,RS, Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15,
 				 Q16, Q17, Q18, Q19, Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, Q28, Q29, Q30, Q31);
