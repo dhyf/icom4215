6af70ccbfc94a35c5f1f40e02f371ac3aef94b50
Eduardo Vazquez Torres <eduardo.vazquez1@upr.edu>
2015-03-10 11:46:41 -0400

    Aqui esta la estructura de Alu, falta trabajarle los ifs pero necesito su ayuda para saber como deseamos q funcione eso. Falta el algoritmo de mult y div Los shifters funcionan para una posicion, vi q segun las notas se hara shift por el numero q el usuario pida ya sea con el # o con un registro. Se puede poner q reciba esa variable q es solo hacer un cambio bobo

--- Alu/alu.v
+++ Alu/alu.v
@@ -1,23 +1,28 @@
-module alu(output [31:0] Y, output [3:0] carryFlag, input [3:0] S,input sign, input [31:0] A,B);
+module alu(output reg [31:0] Y, output [3:0] carryFlag, input [3:0] S,input sign, input [31:0] A,B);
 
 
-output wire carry;
+output reg carry;
 
+
+//carryFlags(0) = V
+//carryFlags(1) = N
+//carryFlags(2) = Z
+//carryFlags(3) = C
 
 
 always@ (S)
 	case (S)
 	4'b0000: Y <= B;
 	4'b0001: assign{carry,Y} = (A + B);
-	4'b0011: Y <= A * B;
+	4'b0011: Y = A * B;
 	4'b0100: Y <= A / B;
 	4'b0101: Y <= A & B;
 	4'b0110: Y <= A | B;
 	4'b0111: Y <= ~(B);
-	4'b1000: Y <= >>B;
-	4'b1001: Y <= <<B;
-	4'b1010: Y <= >>>B;
-	4'b1011: Y <= <<<B;
+	4'b1000: Y = (B>>1);
+	4'b1001: Y = (B<<1);
+	4'b1010: Y = $signed(B) >>> 1;
+	4'b1011: assign{carry,Y} = (A - B);
 	endcase
 
 
