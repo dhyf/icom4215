737ea847ef8beeebc0b98010ac54cd90bec5d2e4
Eduardo Vazquez Torres <eduardo.vazquez1@upr.edu>
2015-03-11 22:16:21 -0400

    Adder con todas las condicionas, Falta hacerle el Test Alu mejorandole la estructura.

--- Adder/adder.v
+++ Adder/adder.v
@@ -1,7 +1,41 @@
-module adder(output [31:0] result, output  C, input [31:0] A,B);
+module adder(output reg [31:0] result, output reg[3:0] carryFlags, input [31:0] A,B, input sign);
 
 
-assign{C,result} = A + B;
+//carryFlags(0) = V
+//carryFlags(1) = N
+//carryFlags(2) = Z
+//carryFlags(3) = C
 
+reg C;
+
+initial begin
+	carryFlags = 4'b0;
+	assign{C,result} = A + B;
+
+		if(sign)
+		begin
+		carryFlags[1] = result[31];
+		carryFlags[3] = C;
+			if(result == 0)
+			begin
+			carryFlags[2] = 1;
+			end
+			if((A[31] == B[31]) && (A[31] != result[31]))
+				begin
+				carryFlags[0] = 1;
+				end
+	    end
+		else 
+			begin
+			carryFlags[0] = 0;
+			carryFlags[1] = 0;
+			carryFlags[3] = C;
+				if(result[31] == 0) 
+					begin
+					carryFlags[2] = 1;
+					end
+	         end 
+	     
+end
 
 endmodule
--- Alu/alu.v
+++ Alu/alu.v
@@ -1,8 +1,10 @@
-module alu(output reg [31:0] Y, output [3:0] carryFlag, input [3:0] S,input sign, input [31:0] A,B);
+module alu(output reg  [31:0] Y, output [3:0] carryFlag, input [3:0] S,input sign, input [31:0] A,B);
 
 
 output reg carry;
-
+  wire [31:0] pHi, pLo;
+  
+  multU mult(pHi, pLo, A,B); //Verificar
 
 //carryFlags(0) = V
 //carryFlags(1) = N
@@ -14,14 +16,14 @@ always@ (S)
 	case (S)
 	4'b0000: Y <= B;
 	4'b0001: assign{carry,Y} = (A + B);
-	4'b0011: Y = A * B;
+	4'b0011: Y = A*B;
 	4'b0100: Y <= A / B;
 	4'b0101: Y <= A & B;
 	4'b0110: Y <= A | B;
 	4'b0111: Y <= ~(B);
-	4'b1000: Y = (B>>1);
-	4'b1001: Y = (B<<1);
-	4'b1010: Y = $signed(B) >>> 1;
+	4'b1000: Y = (B>>A);
+	4'b1001: Y = (B<<A);
+	4'b1010: Y = $signed(B) >>> A;
 	4'b1011: assign{carry,Y} = (A - B);
 	endcase
 
