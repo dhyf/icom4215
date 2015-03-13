af1637528a739771422e27cbde0b66d9bc46231b
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2015-03-13 12:54:24 -0400

    Adding LUI module to ALU, pending testing

--- Alu/alu.v
+++ Alu/alu.v
@@ -7,6 +7,7 @@ module alu(output reg  [31:0] Y, outHI, outLO, output [3:0] carryFlag, input [3:
   	wire [31:0] divisionHI;
   	wire [31:0] divisionLO;
   	wire [31:0] addSubResult;
+  	wire [31:0] luiOutput;
 
 	//carryFlag(0) = V
 	//carryFlag(1) = N
@@ -19,9 +20,13 @@ module alu(output reg  [31:0] Y, outHI, outLO, output [3:0] carryFlag, input [3:
 	//10 -> resta unsigned
 	//11 -> resta signed
   
+
+  	//Instances of modules for multiplication, division, addition, substraction and LUI
   	multU mult(productHI, productLO, sign, A, B);
   	divU div(divisionHI, divisionLO, sign, A, B);
   	adder addSub(addSubResult,carryFlag,A,B,sign);
+  	lui luiModule(luiOutput,A);
+
 
 
 always@ (operation, A, B, sign)
@@ -32,21 +37,23 @@ always@ (operation, A, B, sign)
 	//Sumar o restar (con y sin signo)
 	4'b0001: Y = addSubResult;
 	//Multiplicacion (con y sin signo)
-	4'b0011: begin outHI = productHI; outLO = productLO; end
+	4'b0010: begin outHI = productHI; outLO = productLO; end
 	//Division (con y sin signo)
-	4'b0100: begin outHI = divisionHI; outLO = divisionLO; end
+	4'b0011: begin outHI = divisionHI; outLO = divisionLO; end
 	//AND logico
-	4'b0101: Y <= A & B;
+	4'b0100: Y <= A & B;
 	//OR logico
-	4'b0110: Y <= A | B;
+	4'b0101: Y <= A | B;
 	//NOR logico
-	4'b0111: Y <= ~(A | B);
+	4'b0110: Y <= ~(A | B);
 	//Shift logico a la derecha
-	4'b1000: Y = (B>>A);
+	4'b0111: Y = (B>>A);
 	//Shift lofico a la izquierda
-	4'b1001: Y = (B<<A);
+	4'b1000: Y = (B<<A);
 	//Shift aritmetico a la derecha
-	4'b1010: Y = $signed(B) >>> A;
+	4'b1001: Y = $signed(B) >>> A;
+	//LUI
+	4'b1010: Y = luiOutput;
 	default: Y = Y;
 	endcase
 
