571a5c9e6c6340946eab35161fed5fd2bdf074b3
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2015-03-13 12:42:31 -0400

    Updating ALU to include multiplication, division and adder modules; pending testing

--- Alu/alu.v
+++ Alu/alu.v
@@ -1,34 +1,53 @@
-module alu(output reg  [31:0] Y, output [3:0] carryFlag, input [3:0] S,input sign, input [31:0] A,B);
+module alu(output reg  [31:0] Y, outHI, outLO, output [3:0] carryFlag, input [3:0] operation,input [1:0] sign, input [31:0] A,B);
 
 
-output reg carry;
-  wire [31:0] pHi, pLo;
+  	//Outputs from modules
+  	wire [31:0] productHI;
+  	wire [31:0] productLO;
+  	wire [31:0] divisionHI;
+  	wire [31:0] divisionLO;
+  	wire [31:0] addSubResult;
+
+	//carryFlag(0) = V
+	//carryFlag(1) = N
+	//carryFlag(2) = Z
+	//carryFlag(3) = C
+
+	//Combinaciones de la variable sign:
+	//00 -> suma unsigned
+	//01 -> suma signed
+	//10 -> resta unsigned
+	//11 -> resta signed
   
-  multU mult(pHi, pLo, A,B); //Verificar
-
-//carryFlags(0) = V
-//carryFlags(1) = N
-//carryFlags(2) = Z
-//carryFlags(3) = C
+  	multU mult(productHI, productLO, sign, A, B);
+  	divU div(divisionHI, divisionLO, sign, A, B);
+  	adder addSub(addSubResult,carryFlag,A,B,sign);
 
 
-always@ (S)
-	case (S)
+always@ (operation, A, B, sign)
+
+	case (operation)
+	//Pasa valor B a salida
 	4'b0000: Y <= B;
-	4'b0001: assign{carry,Y} = (A + B);
-	4'b0011: Y = A*B;
-	4'b0100: Y <= A / B;
+	//Sumar o restar (con y sin signo)
+	4'b0001: Y = addSubResult;
+	//Multiplicacion (con y sin signo)
+	4'b0011: begin outHI = productHI; outLO = productLO; end
+	//Division (con y sin signo)
+	4'b0100: begin outHI = divisionHI; outLO = divisionLO; end
+	//AND logico
 	4'b0101: Y <= A & B;
+	//OR logico
 	4'b0110: Y <= A | B;
-	4'b0111: Y <= ~(B);
+	//NOR logico
+	4'b0111: Y <= ~(A | B);
+	//Shift logico a la derecha
 	4'b1000: Y = (B>>A);
+	//Shift lofico a la izquierda
 	4'b1001: Y = (B<<A);
+	//Shift aritmetico a la derecha
 	4'b1010: Y = $signed(B) >>> A;
-	4'b1011: assign{carry,Y} = (A - B);
+	default: Y = Y;
 	endcase
-
-
-
-
 
 endmodule
