6352f4dc2b1a76097131ac6b5b15e7ff94aaefb9
Ernie Rivera Ramos <ernie.rivera@upr.edu>
2015-03-12 23:39:39 -0400

    Divider signed/unsigned done...We need to implement the logic instructions in the ALU and merge it all

--- Divider/divU.v
+++ Divider/divU.v
@@ -1,26 +1,71 @@
 module divU(output reg [31:0] divisionHIRes,
             output reg [31:0] divisionLOQuo,
+            input [1:0] sign,
             input [31:0] dividend, 
             input [31:0] divisor);
   
    //Hacer diagrama de bloque
-   reg [31:0] numToSub;
-   reg [31:0] counter;
-
-    //initialize
-
+   // iverilog -o output.out divU.v divUTest.v
+    reg [31:0] numToSub;
+    reg [31:0] counter;
+    reg [31:0] invDivisor;
+    reg [31:0] invDividend;
 
 
    always @ (dividend, divisor) begin
-      numToSub = dividend;
+      
       counter = 32'd0;
 
+      invDivisor = divisor;
+      invDividend = dividend;
+     
+        if((sign[1]==1) && (dividend[31]==1) && (divisor[31]==1))
+         begin
+               invDivisor = -(invDivisor);
+               $display("if aqui toy");
+               invDividend = -(invDividend);
+         end    
+
+        if((sign[1]==1) && (dividend[31]==1) && (divisor[31]==0))
+         begin
+               //invMultiplier = -(invMultiplier);
+                $display("blah");
+                $display(dividend[31], "       ", divisor[31]);
+               invDividend = -(invDividend);
+
+         end 
+
+           if((sign[1]==1) && (dividend[31]==0) && (divisor[31]==1))
+         begin
+               invDivisor = -(invDivisor);
+                $display("kbron de la guitarra");
+                $display(dividend[31], "       ", divisor[31]);
+               //invDividend = -(invDividend);
+
+         end 
+
+        numToSub = invDividend;
       //dividend - divisor <--- is this less than divisor?
-      while(numToSub >= divisor) begin
+      while(numToSub >= invDivisor) begin
        // $display("Inside loop");   
-        numToSub = numToSub - divisor;
+        numToSub = numToSub - invDivisor;
         counter = counter +1;
       end
+
+        if((sign[1]==1) && (dividend[31]==0) && (divisor[31]==1))
+                begin
+                 $display("Z_Z");
+                  //numToSub = -numToSub;  
+                  counter = -counter;
+                end
+
+
+                if((sign[1]==1) && (dividend[31]==1) && (divisor[31]==0))
+                begin
+                 $display("x_x");
+                  //numToSub = -numToSub;
+                  counter = -counter;
+                end
 
       divisionHIRes = numToSub;
       divisionLOQuo = counter;
--- Divider/divUTest.v
+++ Divider/divUTest.v
@@ -1,14 +1,14 @@
 module divUTest;
   
   //input
-  reg [31:0] dividend=32'd34, divisor = 32'd5;
+  reg [31:0] dividend=32'd34, divisor = -32'd5;
 
 
   //output
   wire [31:0] divisionHIRes;
   wire [31:0] divisionLOQuo;
   
-  divU div(divisionHIRes, divisionLOQuo,dividend,divisor);
+  divU div(divisionHIRes, divisionLOQuo, 2'b10,dividend,divisor);
 
   initial begin
 
@@ -16,6 +16,6 @@ module divUTest;
       
     $display ("\n\tdivisionHIRes\tdivisionLOQuo   \tdivisor \tdividend");
 
-    $monitor ("\n %d  \t%d      \t%d      \t%d", divisionHIRes, divisionLOQuo, divisor, dividend);
+    $monitor ("\n %h  \t%h      \t%h      \t%h", divisionHIRes, divisionLOQuo, divisor, dividend);
   end
 endmodule
