446ef35e6ea0a159283d622974eca03073392e4b
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2015-04-01 17:29:16 -0400

    Adding missing inputs for PC, IR, MDR, MAR, MUXes, etc.

--- Control Unit/controlUnit.v
+++ Control Unit/controlUnit.v
@@ -8,10 +8,17 @@ module controlUnit (output reg regFileRW,
 					output reg [8:0] ramAddress,
 					input [31:0] instruction,
 					input [3:0] aluCarryFlags,
+					input regFileEnable, pcEnable, irEnable, marEnable, mdrEnable,
+					input [1:0] muxSignals, //M0, M1 from data path diagram (mux to ALU(B))
+					input muxSignal, //S from data path diagram (mux to MDR)
 					input ramMFC,
 					input reset,
 					input hardwareInterrupt,
 					input maskableInterrupt);
+
+reg [3:0] state,nextState;
+
+//always @ (instruction, aluCarryFlags, ramMFC, reset,hardwareInterrupt,maskableInterrupt)
 
 
 
