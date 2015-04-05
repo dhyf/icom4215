a9bfb329750b78983b89dfc7de7ae7d708fbf935
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2015-04-04 20:27:33 -0500

    Initial / partial implementation of control unit, fetch and add unisigned. Incomplete CU test

--- Control Unit/controlUnit.v
+++ Control Unit/controlUnit.v
@@ -6,20 +6,137 @@ module controlUnit (output reg regFileRW,
 					output reg ramMFA,
 					output reg ramRW,
 					output reg [8:0] ramAddress,
+					output reg regFileEnable, pcEnable, irEnable, marEnable, mdrEnable,
+					output reg [1:0] muxSignals, //M0, M1 from data path diagram (mux to ALU(B))
+					output reg muxSignals2, //S from data path diagram (mux to MDR)
+					output reg muxSignals3, //Mux selector to load HI/LO registers in mult and div
+					output reg muxSignals4, //Mux selector for imm16, unsigned = 0, sign ext = 1
 					input [31:0] instruction,
 					input [3:0] aluCarryFlags,
-					input regFileEnable, pcEnable, irEnable, marEnable, mdrEnable,
-					input [1:0] muxSignals, //M0, M1 from data path diagram (mux to ALU(B))
-					input muxSignal, //S from data path diagram (mux to MDR)
 					input ramMFC,
 					input reset,
 					input hardwareInterrupt,
-					input maskableInterrupt);
+					input maskableInterrupt,
+					input Clk);
 
-reg [3:0] state,nextState;
+reg [9:0] state,nextState;
 
-//always @ (instruction, aluCarryFlags, ramMFC, reset,hardwareInterrupt,maskableInterrupt)
+wire [5:0] opcode;
+assign opcode = instruction[31:26];
 
+wire [5:0] functionCode;
+assign functionCode = instruction[5:0];
 
+initial begin
+    state=9'd0; //Initial state, no operation? 
+    nextState=9'd1;    
+end
+
+always @ (instruction, aluCarryFlags, ramMFC, reset,hardwareInterrupt,maskableInterrupt, state) begin
+	if(state == 9'd0) begin
+		$display("Inside state 0");
+		nextState=9'd1;
+	end
+
+	else if(state == 9'd1) begin
+		$display("Inside state 1");
+		nextState=9'd2;
+		aluOperation=4'b0000;
+		muxSignals=2'b11;
+		regFileEnable=0;
+		irEnable=0;
+		pcEnable=0;
+		marEnable=1;
+		mdrEnable=0;
+		ramMFA=0;
+	end
+
+	else if(state == 9'd2) begin
+		$display("Inside state 2");
+		nextState=9'd3;
+		aluOperation=4'b1011;
+		muxSignals=2'b11;
+		regFileEnable=0;
+		irEnable=0;
+		pcEnable=1;
+		marEnable=0;
+		mdrEnable=0;
+		ramRW=1;
+		ramMFA=1;
+	end
+
+	else if(state == 9'd3) begin
+		$display("Inside state 3");
+		$display("ramMFC= %b",ramMFC);
+		if(ramMFC) begin
+			$display("Going to state 4");
+			nextState=9'd4;
+			regFileEnable=0;
+			irEnable=0;
+			pcEnable=0;
+			marEnable=0;
+			mdrEnable=0;
+			ramRW=1;
+			ramMFA=1;
+		end
+		else nextState=9'd3;
+	end
+
+	else if(state == 9'd4) begin
+		$display("Inside state 4");
+		nextState=9'd255;
+		regFileEnable=0;
+		irEnable=1;
+		pcEnable=0;
+		marEnable=0;
+		mdrEnable=0;
+		ramRW=1;
+		ramMFA=1;
+	end
+
+	else if(state == 9'd255) begin
+		$display("Inside state 255 (DECODE)");
+		$display("Opcode = %b", opcode);
+		$display("Function Code = %b", functionCode);
+		if(opcode == 6'b000000) begin
+			$display("Inside opcode 000000");
+			if(functionCode == 6'b100000) begin
+				$display("ADDU: Inside function code 100000");
+				nextState=9'd5;
+			end
+			else begin
+				$display("Invalid instruction: function code not found"); //function code not found
+				nextState = 9'd1;
+			end
+		end
+		else begin
+			$display("Invalid instruction: opcode not found"); //Opcode not found
+			nextState = 9'd1;
+		end
+	end
+
+	else if(state == 9'd5) begin
+		$display("ADDU: Inside state 5");
+		nextState = 9'd1;
+		aluOperation=4'b0001;
+		muxSignals=2'b00;
+		regFileRS = instruction[25:21];
+		regFileRT = instruction[20:16];
+		regFileRD = instruction[15:11];
+		regFileEnable=1;
+		irEnable=0;
+		pcEnable=0;
+		marEnable=0;
+		mdrEnable=0;
+		regFileRW=1;
+		ramMFA=0;
+		aluSign=2'b00;
+	end
+
+end
+
+always @(Clk) begin
+    state = nextState;
+end
 
 endmodule
---
+++ Control Unit/controlUnitTest.v
@@ -0,0 +1,67 @@
+module test_control_unit;
+
+	//Inputs
+	reg [31:0] instruction;
+	reg [3:0] aluCarryFlags;
+	reg ramMFC;
+	reg reset;
+	reg hardwareInterrupt;
+	reg maskableInterrupt;
+	reg Clk;
+
+	//Outputs
+	wire regFileRW;
+	wire [4:0] regFileRD, regFileRS, regFileRT;
+	wire [1:0] aluSign;
+	wire [3:0] aluOperation;
+	wire [1:0] ramDataSize;
+	wire ramMFA;
+	wire ramRW;
+	wire [8:0] ramAddress;
+	wire regFileEnable, pcEnable, irEnable, marEnable, mdrEnable;
+	wire [1:0] muxSignals; //M0; M1 from data path diagram (mux to ALU(B))
+	wire muxSignals2; //S from data path diagram (mux to MDR)
+	wire muxSignals3; //Mux selector to load HI/LO registers in mult and div
+	wire muxSignals4; //Mux selector for imm16; unsigned = 0; sign ext = 1
+
+	controlUnit cu (regFileRW,regFileRD,regFileRS,regFileRT,
+		aluSign,aluOperation,ramDataSize,ramMFA,ramRW,
+		ramAddress,regFileEnable,pcEnable,irEnable,marEnable,
+		mdrEnable,muxSignals,muxSignals2,muxSignals3,
+		muxSignals4,instruction,aluCarryFlags,ramMFC,reset,
+		hardwareInterrupt,maskableInterrupt,Clk);
+
+	initial #30 $finish;
+
+	//Clock signal
+	initial begin
+		Clk = 1'b0;
+		forever begin
+			#1 Clk = ~Clk;
+		end
+	end
+
+	
+
+	initial fork
+		#0 instruction = 32'b00000000001000100001100000100000;
+		#0 ramMFC = 0;
+		#3 ramMFC = 1;
+		#10 instruction = 32'b10000010000000000000000000000000;
+	join
+
+
+	initial begin
+
+		$display("\nControl Unit Test");
+		$monitor("\nTime: %d", $time);
+		$monitor("\nOutput: regFileRW: %b", regFileRW);
+		$monitor("\nOutput: regFileRS: %b", regFileRS);
+		$monitor("\nOutput: regFileRT: %b", regFileRT);
+		$monitor("\nOutput: regFileRD: %b", regFileRD);
+		
+	end
+
+endmodule
+
+
